209d4ab19a13b377ddfd91d5d5f7941d0767fb2b
Pamela Erraes <pamela_erraez@hotmail.com>
2025-02-22 19:58:57 -0500

    agregar BlogComponent con formulario y validación para agregar noticias

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { NoticiasService } from '../../services/noticias.service';
+import { INoticia } from '../../interfaces/inoticia.interfaces';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-blog',
@@ -8,14 +12,41 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-  
+  noticiasServices = inject(NoticiasService);
+
   getDataForm(blogForm: NgForm) {
     if (blogForm.invalid) {
-
+      this.mostrarError("Todos los campos son obligatorios.");
+      return;
     }
     
-   
+    let newNoticia: INoticia = blogForm.value;
 
+    let response = this.noticiasServices.insert(newNoticia);
 
-}
+    if (response.status) {
+      this.mostrarExito("Noticia agregada correctamente.");
+      blogForm.reset(); // Limpia el formulario después de agregar la noticia
+    } else {
+      this.mostrarError(response.msg);
+    }
+  }
+
+  mostrarExito(mensaje: string) {
+    Swal.fire({
+      title: "Éxito",
+      text: mensaje,
+      icon: "success",
+      confirmButtonText: "Aceptar"
+    });
+  }
+
+  mostrarError(mensaje: string) {
+    Swal.fire({
+      title: "",
+      text: mensaje,
+      icon: "warning",
+      confirmButtonText: "Aceptar"
+    });
+  }
 }
